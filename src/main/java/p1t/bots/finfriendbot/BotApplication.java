b879c00395d7473ad64cf58ae289fdba6eab4305
Anton Babych <antonbabi13@gmail.com>
2024-02-03 20:03:20 +0200

    Initial commit

---
+++ src/main/java/p1t/bots/finfriendbot/BotApplication.java
@@ -0,0 +1,13 @@
+package p1t.bots.finfriendbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/p1t/bots/finfriendbot/FinFriendBotApplicationTests.java
@@ -0,0 +1,13 @@
+package p1t.bots.finfriendbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinFriendBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
